d8fe22668d28f4f57a7cc7382d33d26a8caafc52
i1-kmv <i1-kmv@mail.ru>
2020-07-17 17:15:11 +0200

    Создал компонент randomChar, создал spinner, создал компонент ошибки

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <>
+        <div className="loadingio-spinner-spinner-5dsmgl7j4hv"><div className="ldio-zdbqqzr6ugc">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>  
+        </>
+    )
+}
+
+export default Spinner;
+
