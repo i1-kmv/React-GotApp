5f8299b2673e8ad25b4196bcacaaa34a01137671
i1-kmv <i1-kmv@mail.ru>
2020-07-19 16:45:18 +0200

    Updates

---
+++ precache-manifest.1f2a1301e0f466b48eb0bec54d5974ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d93baf1481dfca976dd0",
+    "url": "/i1-kmv/React-GotApp/static/css/main.a454bdc2.chunk.css"
+  },
+  {
+    "revision": "d93baf1481dfca976dd0",
+    "url": "/i1-kmv/React-GotApp/static/js/main.d93baf14.chunk.js"
+  },
+  {
+    "revision": "35c9bdb942cdf144e3b7",
+    "url": "/i1-kmv/React-GotApp/static/css/1.123263f5.chunk.css"
+  },
+  {
+    "revision": "35c9bdb942cdf144e3b7",
+    "url": "/i1-kmv/React-GotApp/static/js/1.35c9bdb9.chunk.js"
+  },
+  {
+    "revision": "4c1c034af4e6f7a8e721",
+    "url": "/i1-kmv/React-GotApp/static/js/runtime~main.4c1c034a.js"
+  },
+  {
+    "revision": "a109f76c733ea31a7509bfdd02f05c5e",
+    "url": "/i1-kmv/React-GotApp/static/media/error.a109f76c.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/i1-kmv/React-GotApp/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "e7fa3e201f49e19a7e54767580447eb9",
+    "url": "/i1-kmv/React-GotApp/index.html"
+  }
+];
